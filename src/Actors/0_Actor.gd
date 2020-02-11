53514cfac2cdb4732ec4fc6ac4810f1777278f51
nurizkaf96 <nurizkaf96@gmail.com>
2020-02-11 18:43:45 +0700

    Collision and Movement set

--- src/Actors/0_Actor.gd
+++ src/Actors/0_Actor.gd
@@ -3,7 +3,7 @@ class_name Actor
 
 const FLOOR_NORMAL := Vector2.UP
 
-export var speed := Vector2(1000.0,1000.0)
-export var gravity := 4000.0
+export var speed := Vector2(100.0,100.0)
+export var gravity := 250.0
 
 var _velocity := Vector2.ZERO 
