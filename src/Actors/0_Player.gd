8fdd130350a87c0ad8f58f860d3d9129eff09639
nurizkaf96 <nurizkaf96@gmail.com>
2020-02-11 19:33:22 +0700

    Add 4 Characters Script + Sprite

--- src/Actors/0_Player.gd
+++ src/Actors/0_Player.gd
@@ -1,6 +1,7 @@
 extends Actor
 class_name Player
 
+
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted :=  Input.is_action_just_released("jump") and _velocity.y < 0.0
 	 
@@ -10,9 +11,13 @@ func _physics_process(delta: float) -> void:
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 
 func get_direction() -> Vector2:
+	var move_right := ""
+	var move_left := ""
+	var jump := ""
+	
 	return Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
+		Input.get_action_strength(move_right) - Input.get_action_strength(move_left),
+		-1.0 if Input.is_action_just_pressed(jump) and is_on_floor() else 1.0
 	)
 	
 func calculate_move_velocity(
