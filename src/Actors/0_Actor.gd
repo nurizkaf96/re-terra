1c01253dd7f64f260babfe78a8a6e3d099873ebe
nurizkaf96 <nurizkaf96@gmail.com>
2020-02-14 11:25:51 +0700

    Laying All the Map

--- src/Actors/0_Actor.gd
+++ src/Actors/0_Actor.gd
@@ -3,7 +3,7 @@ class_name Actor
 
 const FLOOR_NORMAL := Vector2.UP
 
-export var speed := Vector2(300.0,400.0)
-export var gravity := 500.0
+export var speed := Vector2(200.0,200.0)
+export var gravity := 250.0
 
 var _velocity := Vector2.ZERO 
