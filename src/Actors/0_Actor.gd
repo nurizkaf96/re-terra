cf0fde5c22639e028e2dc3ac6df785cd8c916f24
nurizkaf96 <nurizkaf96@gmail.com>
2020-02-23 22:08:03 +0700

    Restructure and Delete Everything

--- src/Actors/0_Actor.gd
+++
@@ -1,9 +0,0 @@
-extends KinematicBody2D
-class_name Actor
-
-const FLOOR_NORMAL := Vector2.UP
-
-export var speed := Vector2(200.0,200.0)
-export var gravity := 250.0
-
-var _velocity := Vector2.ZERO 
--- src/Actors/Enemies/Enemies.gd
+++
@@ -1,6 +0,0 @@
-extends 'res://src/Actors/0_Actor.gd'
-
-func _physics_process(delta: float) -> void:
-	if is_on_wall():
-		_velocity.x *= -1
-
